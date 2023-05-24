deba69032d2550a0af93d90d6c1566038ebdcdb1
Prem-Kumar16 <75419846+Prem-Kumar16@users.noreply.github.com>
2023-05-24 20:00:23 +0530

    Update build-vsim.sh

--- scripts/build-vsim.sh
+++ scripts/build-vsim.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+sudo chmod 666 /var/run/docker.sock
+
+# Get ECR login credentials to pull image
+
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/h2b3x9k9
+docker pull public.ecr.aws/h2b3x9k9/vsim:latest
+docker tag public.ecr.aws/h2b3x9k9/vsim:latest vsim:latest
 pushd vsim
-tar -czh . | docker build -t vsim -
-sleep 3
 if [ -e /run/k3s/containerd/containerd.sock ]; then
-docker save vsim:latest | ctr -a /run/k3s/containerd/containerd.sock -n=k8s.io images import -
+docker save vsim:latest | sudo ctr -a /run/k3s/containerd/containerd.sock -n=k8s.io images import -
 fi
 popd
